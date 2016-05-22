eebe64289fca5e162ed16060b5b5eb058a18b314
amite <amit.erandole@gmail.com>
2016-05-22 20:14:00 +0530

    created more components

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,61 @@ var ReactDOM = require('react-dom');
 
 
 
+/*
+  App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header/>
+        </div>  
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+});
+
+/*
+  Header
+  <Header/>
+*/
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+  Order
+  <Order/>
+*/
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory
+  <Inventory/>
+*/
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
 
 // StorePicker
 
@@ -20,4 +75,4 @@ const StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
+ReactDOM.render(<App/>, document.querySelector('#main'))
